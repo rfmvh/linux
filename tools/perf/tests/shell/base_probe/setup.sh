02ca8e6c715fa9f7e88bfff24d6f45cfa5375720
Veronika Molnarova <vmolnaro@redhat.com>
2024-12-19 20:39:07 +0100

    perftool-testsuite: Add empty setup for base_probe
    Add empty setup to set a proper name for base_probe testsuite, can be
    utilized for basic test setup for the future.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>
    Signed-off-by: Veronika Molnarova <vmolnaro@redhat.com>

---
+++ tools/perf/tests/shell/base_probe/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# perftool-testsuite :: perf_probe
+# SPDX-License-Identifier: GPL-2.0
+
+#
+#	setup.sh of perf probe test
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#
+#		Setting testsuite name, for future use
+#
+#
